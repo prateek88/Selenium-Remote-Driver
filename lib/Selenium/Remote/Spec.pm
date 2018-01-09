327c8ff67937b30b846331e9048bec57c223b6ef
George S. Baugh <george@troglodyne.net>
2018-01-08 23:28:47 -0600

    More on #345: make some polyfills to map v3->v2 better

--- lib/Selenium/Remote/Spec.pm
+++ lib/Selenium/Remote/Spec.pm
@@ -26,60 +26,64 @@ extends 'Selenium::Remote::Commands';
 
 #Method    URI Template    no_content_success    internal_name    Command
 our $spec = qq{
-POST    /session                                              0 newSession                   New Session
-DELETE  /session/:sessionId                                   1 quit                         Delete Session
-GET     /status                                               0 status                       Status
-GET     /session/:sessionId/timeouts                          0 getTimeouts                  Get Timeouts
-POST    /session/:sessionId/timeouts                          1 setTimeout                   Set Timeouts
-POST    /session/:sessionId/url                               1 get                          Navigate To
-GET     /session/:sessionId/url                               0 getCurrentUrl                Get Current URL
-POST    /session/:sessionId/back                              1 goBack                       Back
-POST    /session/:sessionId/forward                           1 goForward                    Forward
-POST    /session/:sessionId/refresh                           1 refresh                      Refresh
-GET     /session/:sessionId/title                             0 getTitle Get                 Title
-GET     /session/:sessionId/window                            0 getCurrentWindowHandle       Get Window Handle
-DELETE  /session/:sessionId/window                            1 close                        Close Window
-POST    /session/:sessionId/window                            1 switchToWindow               Switch To Window
-GET     /session/:sessionId/window/handles                    0 getWindowHandles             Get Window Handles
-POST    /session/:sessionId/frame                             1 switchToFrame                Switch To Frame
-POST    /session/:sessionId/frame/parent                      1 switchToParentFrame          Switch To Parent Frame
-GET     /session/:sessionId/window/rect                       0 getWindowSize                Get Window Rect
-POST    /session/:sessionId/window/rect                       1 setWindowSize                Set Window Rect
-POST    /session/:sessionId/window/maximize                   1 maximizeWindow               Maximize Window
-POST    /session/:sessionId/window/minimize                   1 minimizeWindow               Minimize Window
-POST    /session/:sessionId/window/fullscreen                 1 fullscreenWindow             Fullscreen Window
-GET     /session/:sessionId/element/active                    0 getActiveElement             Get Active Element
-POST    /session/:sessionId/element                           0 findElement                  Find Element
-POST    /session/:sessionId/elements                          0 findElements                 Find Elements
-POST    /session/:sessionId/element/:id/element               0 findChildElement             Find Element From Element
-POST    /session/:sessionId/element/:id/elements              0 findChildElement             Find Elements From Element
-GET     /session/:sessionId/element/:id/selected              0 isElementSelected            Is Element Selected
-GET     /session/:sessionId/element/:id/attribute/:name       0 getElementAttribute          Get Element Attribute
-GET     /session/:sessionId/element/:id/property/:name        0 getElementProperty           Get Element Property
-GET     /session/:sessionId/element/:id/css/:propertyName     0 getElementValueOfCssProperty Get Element CSS Value
-GET     /session/:sessionId/element/:id/text                  0 getElementText               Get Element Text
-GET     /session/:sessionId/element/:id/name                  0 getElementTagName            Get Element Tag Name
-GET     /session/:sessionId/element/:id/rect                  0 getElementSize               Get Element Rect
-GET     /session/:sessionId/element/:id/enabled               0 isElementEnabled             Is Element Enabled
-POST    /session/:sessionId/element/:id/click                 1 click                        Element Click
-POST    /session/:sessionId/element/:id/clear                 1 clearElement                 Element Clear
-POST    /session/:sessionId/element/:id/value                 1 sendKeysToElement            Element Send Keys
-GET     /session/:sessionId/source                            0 getPageSource                Get Page Source
-POST    /session/:sessionId/execute/sync                      0 executeScript                Execute Script
-POST    /session/:sessionId/execute/async                     0 executeAsyncScript           Execute Async Script
-GET     /session/:sessionId/cookie                            0 getAllCookies                Get All Cookies
-GET     /session/:sessionId/cookie/:name                      0 getNamedCookie               Get Named Cookie
-POST    /session/:sessionId/cookie                            1 addCookie                    Add Cookie
-DELETE  /session/:sessionId/cookie/:name                      1 deleteCookieNamed            Delete Cookie
-DELETE  /session/:sessionId/cookie                            1 deleteAllCookies             Delete All Cookies
-POST    /session/:sessionId/actions                           1 generalAction                Perform Actions
-DELETE  /session/:sessionId/actions                           1 releaseGeneralAction         Release Actions
-POST    /session/:sessionId/alert/dismiss                     1 dismissAlert                 Dismiss Alert
-POST    /session/:sessionId/alert/accept                      1 acceptAlert                  Accept Alert
-GET     /session/:sessionId/alert/text                        0 getAlertText                 Get Alert Text
-POST    /session/:sessionId/alert/text                        1 sendKeysToPrompt             Send Alert Text
-GET     /session/:sessionId/screenshot                        0 screenshotTake               Screenshot
-GET     /session/:sessionId/element/:id/screenshot            0 elementScreenshot            Take Element Screenshot
+POST    session                                              0 newSession                   New Session
+DELETE  session/:sessionId                                   1 quit                         Delete Session
+GET     status                                               0 status                       Status
+GET     session/:sessionId/timeouts                          0 getTimeouts                  Get Timeouts
+POST    session/:sessionId/timeouts                          1 setTimeout                   Set Page Load timeout (v2->v3 shim)
+POST    session/:sessionId/timeouts/async_script             1 setAsyncScriptTimeout        Set Async script timeout (v2->v3 shim)
+POST    session/:sessionId/timeouts/implicit_wait            1 setImplicitWaitTimeout       Set Implicit wait timeout (v2->v3 shim)
+POST    session/:sessionId/url                               1 get                          Navigate To
+GET     session/:sessionId/url                               0 getCurrentUrl                Get Current URL
+POST    session/:sessionId/back                              1 goBack                       Back
+POST    session/:sessionId/forward                           1 goForward                    Forward
+POST    session/:sessionId/refresh                           1 refresh                      Refresh
+GET     session/:sessionId/title                             0 getTitle Get                 Title
+GET     session/:sessionId/window                            0 getCurrentWindowHandle       Get Currently Focused Window Handle
+DELETE  session/:sessionId/window                            1 close                        Close Currently Focused Window
+POST    session/:sessionId/window                            1 switchToWindow               Switch To Window
+GET     session/:sessionId/window/handles                    0 getWindowHandles             Get Window Handles
+POST    session/:sessionId/frame                             1 switchToFrame                Switch To Frame
+POST    session/:sessionId/frame/parent                      1 switchToParentFrame          Switch To Parent Frame
+GET     session/:sessionId/window/rect                       0 getWindowSize                Get Window Size (v2->v3 shim)
+GET     session/:sessionId/window/rect                       0 getWindowPosition            Get Window Position (v2->v3 shim)
+POST    session/:sessionId/window/rect                       1 setWindowSize                Set Window Size (v2->v3 shim)
+POST    session/:sessionId/window/rect                       1 setWindowPosition            Set Window Position (v2->v3 shim)
+POST    session/:sessionId/window/maximize                   1 maximizeWindow               Maximize Window
+POST    session/:sessionId/window/minimize                   1 minimizeWindow               Minimize Window
+POST    session/:sessionId/window/fullscreen                 1 fullscreenWindow             Fullscreen Window
+GET     session/:sessionId/element/active                    0 getActiveElement             Get Active Element
+POST    session/:sessionId/element                           0 findElement                  Find Element
+POST    session/:sessionId/elements                          0 findElements                 Find Elements
+POST    session/:sessionId/element/:id/element               0 findChildElement             Find Element From Element
+POST    session/:sessionId/element/:id/elements              0 findChildElements            Find Elements From Element
+GET     session/:sessionId/element/:id/selected              0 isElementSelected            Is Element Selected
+GET     session/:sessionId/element/:id/attribute/:name       0 getElementAttribute          Get Element Attribute
+GET     session/:sessionId/element/:id/property/:name        0 getElementProperty           Get Element Property
+GET     session/:sessionId/element/:id/css/:propertyName     0 getElementValueOfCssProperty Get Element CSS Value
+GET     session/:sessionId/element/:id/text                  0 getElementText               Get Element Text
+GET     session/:sessionId/element/:id/name                  0 getElementTagName            Get Element Tag Name
+GET     session/:sessionId/element/:id/rect                  0 getElementSize               Get Element Rect
+GET     session/:sessionId/element/:id/enabled               0 isElementEnabled             Is Element Enabled
+POST    session/:sessionId/element/:id/click                 1 clickElement                 Element Click
+POST    session/:sessionId/element/:id/clear                 1 clearElement                 Element Clear
+POST    session/:sessionId/element/:id/value                 1 sendKeysToElement            Element Send Keys
+GET     session/:sessionId/source                            0 getPageSource                Get Page Source
+POST    session/:sessionId/execute/sync                      0 executeScript                Execute Script
+POST    session/:sessionId/execute/async                     0 executeAsyncScript           Execute Async Script
+GET     session/:sessionId/cookie                            0 getAllCookies                Get All Cookies
+GET     session/:sessionId/cookie/:name                      0 getNamedCookie               Get Named Cookie
+POST    session/:sessionId/cookie                            1 addCookie                    Add Cookie
+DELETE  session/:sessionId/cookie/:name                      1 deleteCookieNamed            Delete Cookie
+DELETE  session/:sessionId/cookie                            1 deleteAllCookies             Delete All Cookies
+POST    session/:sessionId/actions                           1 generalAction                Perform Actions
+DELETE  session/:sessionId/actions                           1 releaseGeneralAction         Release Actions
+POST    session/:sessionId/alert/dismiss                     1 dismissAlert                 Dismiss Alert
+POST    session/:sessionId/alert/accept                      1 acceptAlert                  Accept Alert
+GET     session/:sessionId/alert/text                        0 getAlertText                 Get Alert Text
+POST    session/:sessionId/alert/text                        1 sendKeysToPrompt             Send Alert Text
+GET     session/:sessionId/screenshot                        0 screenshot                   Take Screenshot
+GET     session/:sessionId/element/:id/screenshot            0 elementScreenshot            Take Element Screenshot
 };
 
 our $spec_parsed;
@@ -93,7 +97,7 @@ sub get_spec {
         $spec_parsed->{$key} = {
            method             => $method,
            url                => $uri,
-           no_content_success => int($nc_success),
+           no_content_success => int($nc_success), #XXX this *should* always be 0, but specs lie
            description        => join(' ',@description),
         };
     }
@@ -105,6 +109,26 @@ has '_cmds' => (
     reader  => 'get_cmds',
     builder => \&get_spec,
 );
+
+sub get_params {
+    my ( $self, $args ) = @_;
+    if ( !( defined $args->{'session_id'} ) ) {
+        return;
+    }
+    my $data    = $self->SUPER::get_params($args);
+
+    #URL & data polyfills for the way selenium2 used to do things, etc
+    $data->{payload} = {};
+    $data->{payload}->{pageLoad} = $args->{value} if $data->{url} =~ m/timeouts$/;
+    $data->{payload}->{script}   = $args->{value} if $data->{url} =~ s/timeouts\/async_script$/timeouts/g;
+    $data->{payload}->{implicit} = $args->{value} if $data->{url} =~ s/timeouts\/implicit_wait$/timeouts/g;
+    #$data->{payload}->{handle}   = $args->{value} if $args->{command} eq 'switchToWindow'; #XXX probably not needed? lets hope not
+    $data->{payload}->{handle}   = $args->{window_handle} if grep { $args->{command} eq $_ } qw{setWindowSize getWindowSize setWindowPosition getWindowPosition fullscreenWindow minimizeWindow maximizeWindow};
+    return $data;
+}
+
+
+#Utility
 
 sub get_spec_differences {
     my $v2_spec = Selenium::Remote::Commands->new()->get_cmds();

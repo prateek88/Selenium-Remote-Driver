637920a9f32e28b2561434b44cf09d85466cbf53
George S. Baugh <george@troglodyne.net>
2018-01-23 10:07:22 -0600

    Even more workaround fiddling, this time with submit()

--- lib/Selenium/Remote/WebElement.pm
+++ lib/Selenium/Remote/WebElement.pm
@@ -133,6 +133,10 @@ sub click {
     Submit a FORM element. The submit command may also be applied to any element
     that is a descendant of a FORM element.
 
+ Compatibility:
+    On webdriver3 enabled servers, this uses a JS shim, which may not submit correctly depending on the element you are attempting to submit.
+    Try clicking it if possible instead.
+
  Usage:
     $elem->submit();
 
@@ -140,7 +144,7 @@ sub click {
 
 sub submit {
     my ($self) = @_;
-    return $self->driver->execute_script("return arguments[0].submit();", {'element-6066-11e4-a52e-4f735466cecf'=> $self->{id}} ) if $self->driver->{is_wd3} && !(grep { $self->driver->browser_name eq $_ } qw{chrome MicrosoftEdge});
+    return $self->driver->execute_script("if (typeof arguments[0].submit === 'function') { return arguments[0].submit(); }; return 0;", {'element-6066-11e4-a52e-4f735466cecf'=> $self->{id}} ) if $self->driver->{is_wd3} && !(grep { $self->driver->browser_name eq $_ } qw{chrome MicrosoftEdge});
     my $res = { 'command' => 'submitElement', 'id' => $self->id };
     return $self->_execute_command($res);
 }
